bb87466a5def43c61acfd3a6ba1b3c91c45b30fb
Bjørn Rudi Dahl <bjorn.rudi.dahl@biimultrasound.com>
2022-03-27 18:58:17 +0200

    Changing press-and-hold duration from 2000ms to 250ms

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QStyleHints>
 
 
 int main(int argc, char *argv[])
@@ -7,6 +8,7 @@ int main(int argc, char *argv[])
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
     QGuiApplication app(argc, argv);
+   app.styleHints()->setMousePressAndHoldInterval(250);
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/WarzoneRoulette/main.qml"_qs);
